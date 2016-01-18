db5738ec6d82ec56e7586bcb2e0cfe559d253dd3
Cyrus Cousins <originalcyruscousins@gmail.com>
2016-01-18 01:34:53 -0500

    Utility to time program execution.

--- src/Rosalyn/Executor.hs
+++ src/Rosalyn/Executor.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, DefaultSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, DefaultSignatures, ExistentialQuantification #-}
 module Rosalyn.Executor where
 
 --Logic imports
 import Data.Hashable
 import Data.List
+import Data.List.Split
 import Data.Word
 import Numeric (showHex)
 
@@ -29,17 +30,45 @@ import Control.Monad
 memoizationRoot :: String
 memoizationRoot = "memo"
 
+--Calculate arbitrary length hashes:
+--Concatenate an arbitrary number of these values together to produce a hash of arbitrary length.
+hashMulti :: (Hashable a) => a -> [Word]
+hashMulti a = map (\ s -> fromIntegral $ hashWithSalt s a) [0..]
+
+--Produce a multi word hash hexidecimal string.
+hashStringNWords :: (Hashable a) => Int -> a -> String
+hashStringNWords n a =
+  let words :: [Word]
+      words = (take n) (hashMulti a)
+   in showHex (wordsToInteger words) ""
+
+--Convert a word array to an integer that uniquely represents the array.
+wordsToInteger :: [Word] -> Integer
+wordsToInteger words =
+  let wordSize :: Integer
+      wordSize = fromIntegral (maxBound :: Word)
+   in foldr (\ w i -> i * wordSize + (fromIntegral w)) 0 words
+
+--Produce a 256 bit hash, represented as a hex string.
+hashString256 :: (Hashable a) => a -> String
+hashString256 =
+  let wordsTo256 :: Word
+      wordsTo256 = div 256 maxBound
+   in hashStringNWords (fromIntegral wordsTo256)
+
+
 --class Executable p where
 --  type a :: * -> * --input type.
 --  type b :: * -> * --output type.
 
 class Executable p a b | p -> a b where
   --Inherent properties of a program (constants).
-  binaryName :: p -> String --StringConstant (a, b)
-  subName :: p -> String --StringConstant (a, b)
-  --isStochastic :: Bool
-  --isStochastic = False
-  programDirectory :: p -> String --StringConstant a
+  binaryName :: p -> String
+  subName :: p -> String
+  subName _ = ""
+  --isStochastic :: p -> Bool
+  --isStochastic _ = False
+  programDirectory :: p -> String
   programDirectory p = (intercalate "/" [memoizationRoot, binaryName p, subName p]) ++ "/"
   executionSubdirectory :: p -> a -> String
   default executionSubdirectory :: (Hashable p, Hashable a) => p -> a -> String
@@ -47,7 +76,7 @@ class Executable p a b | p -> a b where
     let p32 :: Word32
         p32 = fromIntegral $ hash p
         a32 :: Word32
-        a32 = fromIntegral $ hash a
+        a32 = fromIntegral $ hash a --TODO use hashString256
      in (showHex p32 "") ++ " " ++ (showHex a32 "")
   fullDirectory :: p -> a -> String
   fullDirectory p a = (programDirectory p) ++ (executionSubdirectory p a) ++ "/"
@@ -81,6 +110,7 @@ class Executable p a b | p -> a b where
     let binName = (binaryName p)
         dir = fullDirectory p a
         pdir = programDirectory p
+        --TODO create a reserved directory for these files.
         stdInFile = dir ++ "stdin"
         stdOutFile = dir ++ "stdout"
         stdErrFile = dir ++ "stderr"
@@ -112,7 +142,7 @@ class Executable p a b | p -> a b where
            (_, _, _, process) <- createProcess (CreateProcess { cmdspec = (RawCommand binName args), cwd = (Just (cwd ++ "/" ++ dir)), env = Nothing, std_in = (UseHandle stdInStream), std_out = (UseHandle stdOutStream), std_err = (UseHandle stdErrStream), close_fds = False, create_group = False, delegate_ctlc = False }) ; -- Should try to isolate the process as much as possible. --TODO may need to set the path.
            --Wait for the process to terminate.
            waitForProcess process ;
-           --Create a file to signal that the irectory contains a valid memo.
+           --Create a file to signal that the directory contains a valid memo.
            writeFile completeFile "" ;
   readProgramResult :: p -> a -> IO b
   readProgramResult p a = --TODO this allows data races when used with unsafeIO.  We need a way of encapsulating the CWD aspects of the code.
@@ -130,6 +160,23 @@ class Executable p a b | p -> a b where
            readProgramResult p a ;
   runProgramUnsafe :: p -> a -> b
   runProgramUnsafe p a = unsafePerformIO (runProgram p a)
+  --Query additional aspects of a program run.  These are generally backdoors, and used for less reputable purposes.
+  readExecutionArtifact :: p -> a -> FilePath -> IO (Maybe String)
+  readExecutionArtifact p a f =
+    let dir = executionSubdirectory p a
+        f' = dir ++ f
+     in do e <- doesFileExist f'
+           if e then fmap Just (readFile f') else return Nothing
+  readExecutionArtifactUnsafe :: p -> a -> FilePath -> Maybe String
+  readExecutionArtifactUnsafe p a f = unsafePerformIO $ readExecutionArtifact p a f
+  readStdout :: p -> a -> IO (Maybe String)
+  readStdout p a = readExecutionArtifact p a "stdout"
+  readStdoutUnsafe :: p -> a -> Maybe String
+  readStdoutUnsafe p a = unsafePerformIO $ readStdout p a
+  readStderr :: p -> a -> IO (Maybe String)
+  readStderr p a = readExecutionArtifact p a "stderr"
+  readStderrUnsafe :: p -> a -> Maybe String
+  readStderrUnsafe p a = unsafePerformIO $ readStderr p a
   --Clean a directory used in a single run.
   --cleanRun :: p -> a -> IO ()
   --TODO: Need to make sure this doesn't follow symlinks.  It isn't supposed to, but I've read some bug reports.  Seeing as this would wipe out the entire program directory due to origin, this is a risk I'd not take lightly.
@@ -138,6 +185,25 @@ class Executable p a b | p -> a b where
   --cleanProgram :: p -> a -> IO ()
   --cleanProgram p a = removeDirectoryRecursive (programDirectory p a)
   --TODO: Interface for removing intermediate results from a program directory.
+
+--Wrapper to time programs.  Uses /usr/bin/time, assuming availability of the -v and -o options.
+--TODO we could also make this a subclass with all default functions available.
+--data TimedExecutable p a b = forall p a b . (Executable p a b) => TimedExecutable p
+data TimedExecutable p a b = TimedExecutable p
+
+instance (Executable p a b) => Executable (TimedExecutable p a b) a (b, [(String, String)]) where
+  binaryName _ = "/usr/bin/time"
+  arguments (TimedExecutable p) a = ["-v", "-o", "timeinfo", binaryName p] ++ (arguments p a)
+  programDirectory (TimedExecutable p) = (programDirectory p) ++ "timed/" 
+  executionSubdirectory (TimedExecutable p) = executionSubdirectory p
+  fullDirectory (TimedExecutable p) a = (programDirectory p) ++ (executionSubdirectory p a) ++ "/"
+  readProgramResult (TimedExecutable p) a =
+    do result <- readProgramResult p a
+       timeInfo <- (readFile "timeinfo")
+       return (result, map (\ [a, b] -> (a, b)) $ filter (\ x -> length x == 2) $ map (splitOn ": ") $ lines timeInfo)
+  inputToString (TimedExecutable p) a = inputToString p a
+  outputFromString (TimedExecutable p) b = undefined -- outputFromString p b
+
 {-
 data DynamicExecutable = DynamicExecutable 
 
